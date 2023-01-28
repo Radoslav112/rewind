7440c65c2eddd9d820e8ed70b71433b7ff4b6950
unknown <radiboy112@gmail.com>
2023-01-29 00:52:24 +0200

    implement page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -9,6 +10,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'page-not-found', component: ErrorPageComponent },
+  { path: '**', redirectTo: 'page-not-found' },
 ];
 
 @NgModule({
